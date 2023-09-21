d8063b5ac6c4607ee2c6473181f7f33fc3d5b14c
Chysmm <som4nick@yahoo.com>
2023-09-21 11:56:39 -0700

    4_add.c

--- 4_add.c
+++ 4_add.c
@@ -14,7 +14,7 @@ UNUSED(stack);
 
 if (args->stack_len < 2)
 {
-printf(2, "L%d: can't add, stack too short\n", line_num);
+dprintf(2, "L%d: can't add, stack too short\n", line_num);
 free_all_args();
 exit(EXIT_FAILURE);
 }
